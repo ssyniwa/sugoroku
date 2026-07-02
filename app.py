9a8b9c345ad7516f34a835a95f704419de80fe4e
SyouyaNiwa <109267191+ssyniwa@users.noreply.github.com>
2026-07-02 16:33:18 +0900

    Fix formatting of event entries in app.py

--- app.py
+++ app.py
@@ -41,7 +41,7 @@ STUDENT_EVENTS = [
     {"text": "スマホを壊して修理代", "img": "images/broken.png", "money": -15},
     {"text": "テストで赤点、補習費", "img": "images/fail.png", "money": -5},
     {"text": "友達と食べ歩きしすぎた", "img": "images/eat.png", "money": -5},
-    {"text": "図書カードをもらった", "img": "images/book.png", "money": 5},
+    {"text": "図書カードをもらった", "img": "images/book.png", "money": 5}
 ]
 SALARYMAN_EVENTS = [
     {"text": "プロジェクトリーダーに抜擢！", "img": "images/job11.png", "money": 30},
@@ -63,7 +63,7 @@ SALARYMAN_EVENTS = [
     {"text": "健康診断で再検査の診断", "img": "images/job116.png", "money": -15},
     {"text": "新しいスーツを新調", "img": "images/job117.png", "money": -20},
     {"text": "プチーム目標達成で報奨金", "img": "images/job118.png", "money": 20},
-    {"text": "慣れない事務処理でミス連発", "img": "images/job119.png", "money": -10}, ... 他の18種をここに追加
+    {"text": "慣れない事務処理でミス連発", "img": "images/job119.png", "money": -10} ... 他の18種をここに追加
 ]
 JOB_EVENTS = [
     {"text": "残業続きで疲弊", "img": "images/work_hard.png", "money": -10},
